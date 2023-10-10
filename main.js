95db7bcfd6e2465c954fc21cc7c8e75af6623e20
thalisson <thalissonboth9@gmail.com>
2023-10-10 19:21:03 -0300

    Finalizando o projeto

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
-form = document.getElementById('form');
+const listaDeContatos = []; 
+const tabela = document.getElementById("tabela");
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    const nome = document.getElementById('nome').value
-    const contato = document.getElementById('numero').value
-    adicionar(nome, contato);
-})
-
-function adicionar(nome, contato){
-    const corpo = document.querySelector('tbody')
+    const nome = document.getElementById('nome').value;
+    const contato = document.getElementById('numero').value;
     
-    let linha = `<tr>`
-    linha += `<td>${nome}</td>`
-    linha += `<td>${contato}</td>`
-    linha += `</tr>`
-    corpo.innerHTML += linha;
+    if (verificarNomeRepetido(nome)) {
+        alert("Este nome já foi adicionado à lista de contatos.");
+        return;
+    }
+
+    if (verificarTamanhoTelefone(contato)) {
+        alert("O número de telefone está invalido.");
+        return;
+    }
+
+    adicionar(nome, contato);
+});
+
+function verificarNomeRepetido(nome) {
+    return listaDeContatos.includes(nome);
 }
+
+function verificarTamanhoTelefone(contato) {
+    return contato.length < 11;
+}
+
+function adicionar(nome, contato) {
+    const corpo = document.querySelector('tbody');
+
+    let linha = `<tr>`;
+    linha += `<td>${nome}</td>`;
+    linha += `<td>${contato}</td>`;
+    linha += `</tr>`;
+    corpo.innerHTML += linha;
+    tabela.style.display = "flex";
+
+    listaDeContatos.push(nome); 
+}
+
+
 
